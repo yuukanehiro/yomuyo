6c1d95d820899ef87a501fff8e02f30490d6a7f2
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-07-14 12:16:48 +0000

    Dockerfile Copy

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -8,7 +8,7 @@
    <span class="icon-bar"></span>
    </button>
 
-   <a class="navbar-brand" href="/">yomuyo</a>
+   <a class="navbar-brand" href="/"><img src="{{ asset('/images/yomuyo_logo.gif') }}"></a>
   </div>
   <div class="collapse navbar-collapse" id="navbarEexample2">
    <ul class="nav navbar-nav">
