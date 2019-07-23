716a6ff7c680f907f6f59a1d0990d3261257c054
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-07-23 06:24:47 +0000

    Seed Add

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -24,8 +24,34 @@ class BooksTableSeeder extends Seeder
             "google_book_id" => "PzAargEACAAJ",
             "name"            => "まんがでわかる 7つの習慣",
             "thumbnail"        => "PzAargEACAAJ.jpg",
-            "created_at"      => "2019-07-22 01:30:24",
-            "updated_at"      => "2019-07-22 01:30:24",
+            "created_at"      => "2019-07-22 01:32:24",
+            "updated_at"      => "2019-07-22 01:32:24",
         ]);
+        DB::table('books')->insert([
+            "id"              => 3,
+            "google_book_id" => "iGn6ugEACAAJ",
+            "name"            => "Docker/Kubernete",
+            "thumbnail"        => "iGn6ugEACAAJ.jpg",
+            "created_at"      => "2019-07-22 01:33:24",
+            "updated_at"      => "2019-07-22 01:33:24",
+        ]);
+        DB::table('books')->insert([
+            "id"              => 4,
+            "google_book_id" => "ViEzrgEACAAJ",
+            "name"            => "アルジャーノンに花束を",
+            "thumbnail"        => "iGn6ugEACAAJ.jpg",
+            "created_at"      => "2019-07-22 01:34:24",
+            "updated_at"      => "2019-07-22 01:34:24",
+        ]);
+        DB::table('books')->insert([
+            "id"              => 5,
+            "google_book_id" => "ZsmcBgAAQBAJ",
+            "name"            => "［新釈］講孟余話",
+            "thumbnail"        => "ZsmcBgAAQBAJ.jpg",
+            "created_at"      => "2019-07-22 01:35:24",
+            "updated_at"      => "2019-07-22 01:35:24",
+        ]);
+
+
     }
 }
--- database/seeds/ReviewsTableSeeder.php
+++ database/seeds/ReviewsTableSeeder.php
@@ -28,6 +28,35 @@ class ReviewsTableSeeder extends Seeder
             "comment"        => "まんがという形なのでどうやって7つの習慣を使ったらよいか、具体的に学ぶことができる。",
             "created_at"     => "2019-07-22 01:30:24",
             "updated_at"     => "2019-07-22 01:30:24",
-        ]); 
+        ]);
+        DB::table('reviews')->insert([
+            "book_id"        => 3,
+            "user_id"        => 1,
+            "netabare_flag"  => 1,
+            "user_ip"        => "1.1.1.1",
+            "comment"        => "Dockerの基本からKubernetesの開発までの流れを解説している。EKSについて特集して欲しかった。",
+            "created_at"     => "2019-07-22 01:30:24",
+            "updated_at"     => "2019-07-22 01:30:24",
+        ]);
+        DB::table('reviews')->insert([
+            "book_id"        => 4,
+            "user_id"        => 1,
+            "netabare_flag"  => 0,
+            "user_ip"        => "1.1.1.1",
+            "comment"        => "アルジャーノンはネズミである。",
+            "created_at"     => "2019-07-22 01:30:24",
+            "updated_at"     => "2019-07-22 01:30:24",
+        ]);
+        DB::table('reviews')->insert([
+            "book_id"        => 5,
+            "user_id"        => 1,
+            "netabare_flag"  => 1,
+            "user_ip"        => "1.1.1.1",
+            "comment"        => "狂うこと。枠から外れ広がるのだ。",
+            "created_at"     => "2019-07-22 01:30:24",
+            "updated_at"     => "2019-07-22 01:30:24",
+        ]);
+
+ 
     }
 }
