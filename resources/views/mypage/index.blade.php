e0ed1531f96c3b5215176e76fc94d974bf652fef
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-08-02 20:27:49 +0000

    Google Books 画像の通信をhttpsに修正

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -158,7 +158,7 @@
              <div class="row">
                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
                  <a href="/book/detail?id={{ $review->google_book_id }}&thumbnail={{ $review->thumbnail }}&title={{ $review->book_title }}">
-                         <img class="img-thumbnail" src="http://s3.yomuyo.net/books/{{ $review->thumbnail }}" alt="{{ $review->book_title }}">
+                         <img class="img-thumbnail" src="https://s3.yomuyo.net/books/{{ $review->thumbnail }}" alt="{{ $review->book_title }}">
                  </a>
                </div>
                <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
