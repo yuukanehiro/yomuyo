50b0275852899f7f38aaddebc42e02b8cb2e6073
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-08-05 05:17:13 +0000

    いいねボタンのURL対応2

--- public/js/niceAjax.js
+++ public/js/niceAjax.js
@@ -18,13 +18,13 @@ $(function(){
         var $this         = $(this);
         var review_id     = $this.parent('.post').data('review_id');
         var login_user_id = $this.parent('.post').data('login_user_id');
-        var url_root      = $this.parent('.post').data('url_root');
+        //var url_root      = $this.parent('.post').data('url_root');
 
         // Ajax設定 ============================================================================
         $.ajax({
                     type:     'POST',
                     datatype: 'json',
-                    url:      url_root + '/nice/create',
+                    url:      './nice/create',
                     timeout:  3000,
                     data: {
                               _token:    CSRF_TOKEN,       // CSRFトークン
