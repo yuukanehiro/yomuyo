a558f290c327740076ee80b093c8a0391e8a2e1b
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-08-11 17:22:06 +0000

    ログインエラーメッセージ 日本語化対応

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '該当するアカウント情報がありません。',
+    'throttle' => 'ログイン失敗。しばらく待ってからログインしてください。',
+
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
-                        <div class="card-header"><b>{{ __('Login') }}</b></div>
+                        <div class="card-header">{{ __('Login') }}</div>
                         <div class="card-body">
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
