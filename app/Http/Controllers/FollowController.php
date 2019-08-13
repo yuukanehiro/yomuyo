321d4d970bfc4d81f8496f719c7732603570c220
yuukanehiro <kanehiro@yuulinux.tokyo>
2019-08-13 06:37:05 +0000

    フォローボタンのAjaxまで

---
+++ app/Http/Controllers/FollowController.php
@@ -0,0 +1,22 @@
+<?php
+
+  namespace App\Http\Controllers;
+
+  use Illuminate\Http\Request;
+  use Response;
+  use Log;
+  use App\Models\Following;
+  use App\Models\Followed;
+
+class FollowController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+}
